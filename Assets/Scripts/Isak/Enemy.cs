260ff2ff2cee98c444da99e4de894e17e069d2d4
Isak Lönn <isak.lonn@futuregames.nu>
2021-04-19 12:29:36 +0200

    made so the enemies now run away when scared

--- Assets/Scripts/Isak/Enemy.cs
+++ Assets/Scripts/Isak/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : Obstacle
 {
     [SerializeField] int health = 1;
+    [SerializeField] float despawnTimer = 1;
     bool isRunningAway = false;
 
     protected override void Update()
@@ -14,7 +15,13 @@ public class Enemy : Obstacle
     }
     public void FleeFromPlayer(Vector3 playerPosition)
     {
-        body.velocity = -playerPosition;
+        body.velocity = playerPosition;
         isRunningAway = true;
+        StartCoroutine(RunAway());
+    }
+    IEnumerator RunAway()
+    {
+        yield return new WaitForSeconds(despawnTimer);
+        Destroy(gameObject); // use objectpooling instead?
     }
 }
