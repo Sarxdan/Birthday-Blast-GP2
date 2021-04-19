9e4ace82195494b078489f990106738c10cedede
Viktor Ek <72790285+Viktor753@users.noreply.github.com>
2021-04-19 13:29:21 +0200

    Started implementing a Boss
    The setup of a boss is there,
    but the mechanics and what the boss does during certain phases/ states needs to be implemented

---
+++ Assets/Boss/Boss.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public BossPhase[] bossPhases;
+
+    private int currentSphase = 0;
+    
+    
+    private float currPhaseTimer = 0.0f;
+    private bool inPhase;
+
+    [HideInInspector] public Transform playerTarget;
+
+
+    private void Start()
+    {
+        playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        StartBoss();
+    }
+
+    public void StartBoss() 
+    {
+        Debug.Log("Boss has begun!");
+        inPhase = true;
+    }
+
+    public void EndBoss()
+    {
+        Debug.Log("Boss is over");
+        Destroy(gameObject);
+    }
+
+    private void NextPhase()
+    {
+        currentSphase++;
+        inPhase = true;
+        Debug.Log("Started next phase!");
+    }
+
+    private IEnumerator EndPhase()
+    {
+        Debug.Log("End of phase : " + bossPhases[currentSphase]);
+        inPhase = false;
+        currPhaseTimer = 0.0f;
+
+        Debug.Log("Time until next phase : " + bossPhases[currentSphase].phaseDowntime);
+        yield return new WaitForSeconds(bossPhases[currentSphase].phaseDowntime);
+        if (currentSphase == bossPhases.Length - 1)
+        {
+            EndBoss();
+        }
+        else
+        {
+            NextPhase();
+        }
+    }
+    
+    private void Update()
+    {
+        if (inPhase)
+        { 
+            bossPhases[currentSphase].DoPhaseMechanics(this);
+            currPhaseTimer += Time.deltaTime;
+            if (currPhaseTimer >= bossPhases[currentSphase].phaseDuration)
+            {
+                StartCoroutine(EndPhase());
+            }
+        }   
+    }
+}
