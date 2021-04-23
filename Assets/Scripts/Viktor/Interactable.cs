007b45e98b050ceca4e08295519d0a5351ed2f7c
Isak Lönn <isak.lonn@futuregames.nu>
2021-04-23 10:50:17 +0200

    added a way to unlock and use new items

--- Assets/Scripts/Viktor/Interactable.cs
+++ Assets/Scripts/Viktor/Interactable.cs
@@ -53,20 +53,27 @@ public class Interactable : MonoBehaviour
     {
 
         bool meetsReqiurements = true;
-        
-        if(requiredItems.jetpack != Gamemanager.instance.UnlockedItems.jetpack) 
-        {       
-            meetsReqiurements = false;
-        }
-        if(requiredItems.pewpew != Gamemanager.instance.UnlockedItems.pewpew)
+        if(requiredItems.jetpack)
         {
-            meetsReqiurements = false;
+            if(!Gamemanager.instance.UnlockedItems.jetpack)
+            {
+                meetsReqiurements = false;
+            }
         }
-        if(requiredItems.shovel != Gamemanager.instance.UnlockedItems.shovel)
+        if(requiredItems.pewpew)
         {
-            meetsReqiurements = false;
+            if(!Gamemanager.instance.UnlockedItems.pewpew)
+            {
+                meetsReqiurements = false;
+            }
         }
-        
+        if(requiredItems.shovel)
+        {
+            if(!Gamemanager.instance.UnlockedItems.shovel)
+            {
+                meetsReqiurements = false;
+            }
+        }
         return meetsReqiurements;
     }
 }
