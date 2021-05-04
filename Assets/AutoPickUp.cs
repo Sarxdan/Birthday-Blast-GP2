a9da8f208c563c2f9037d490bcbb0ce3618550ab
Viktor Ek <72790285+Viktor753@users.noreply.github.com>
2021-05-04 13:06:52 +0200

    AutoPickUp component

---
+++ Assets/AutoPickUp.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPickUp : MonoBehaviour
+{
+    public Resource resource;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Inventory.instance.PickUpResource(resource);
+        }
+    }
+}
