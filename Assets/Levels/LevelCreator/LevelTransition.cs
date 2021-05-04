53e8314922627dc7fa1c1bc8aceb8ee3f9cb8517
Viktor Ek <72790285+Viktor753@users.noreply.github.com>
2021-05-04 14:26:11 +0200

    Visual transition from Island to new level
    Island to new level visuals

--- Assets/Levels/LevelCreator/LevelTransition.cs
+++ Assets/Levels/LevelCreator/LevelTransition.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
@@ -10,18 +12,58 @@ public class LevelTransition : MonoBehaviour
     public static Events.TransitionEvent onLevelTransit;
     [Header("Insert LVL ID of the level you want to transition TO")]
     public int nextLevel;
+    public float transitionDuration = 1.5f;
+
+    [Header("If going from Island => new level")]
+    public Transform directionOfLaunch;
+    public float launchForce = 100;
 
     public void LoadScene()
     {
+        if (FindObjectOfType<Level>().levelType == LevelType.Island)
+        {
+            
+            //Island => Jetpack/Island
+            
+            var player = FindObjectOfType<ThirdPersonController>().gameObject;
+            var launchDir = directionOfLaunch.forward;
+            var cineCam = FindObjectOfType<CinemachineVirtualCamera>();
+            cineCam.LookAt = player.GetComponentInChildren<Rigidbody>().transform;
+
+            foreach (var scripts in player.GetComponentsInChildren<MonoBehaviour>())
+            {
+                scripts.enabled = false;
+            }
+
+            foreach (var animators in player.GetComponentsInChildren<Animator>())
+            {
+                animators.enabled = false;
+            }
+
+            foreach (var rigidbodies in player.GetComponentsInChildren<Rigidbody>())
+            {
+                rigidbodies.AddForce(launchDir * launchForce);
+            }
+
+            foreach (var colliders in player.GetComponentsInChildren<Collider>())
+            {
+                colliders.enabled = false;
+            }
+        }
+        else
+        {
+            //Jetpack => jetpack/Island
+        }
+
         PlayerPrefs.SetInt("TransitionLevelID",nextLevel);
         var scene = SceneManager.GetActiveScene();
-        /*
-        if(onLevelTransit != null)
-        {
-            onLevelTransit(scene.buildIndex);
-        }
-        */
-        SceneManager.LoadScene(scene.buildIndex);
+        StartCoroutine(WaitBeforeLoading(scene));
+    }
+
+    private IEnumerator WaitBeforeLoading(Scene sceneToLoad)
+    {
+        yield return new WaitForSeconds(transitionDuration);
+        SceneManager.LoadScene(sceneToLoad.buildIndex);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,5 +72,10 @@ public class LevelTransition : MonoBehaviour
         {
             LoadScene();
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(directionOfLaunch.position, directionOfLaunch.forward);
     }
 }
