3fc49bfcb2529dc0ae87451da077dc038d1f4440
Isak Lönn <isak.lonn@futuregames.nu>
2021-05-10 11:06:18 +0200

    quick fix so ui also works after game over

--- Assets/Scripts/Isak/UI/GameOver.cs
+++ Assets/Scripts/Isak/UI/GameOver.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
+    public static Events.EmptyEvent onGameRestart;
     [SerializeField] float loadTime = 1;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,10 @@ public class GameOver : MonoBehaviour
     IEnumerator RestartGame()
     {
         yield return new WaitForSeconds(loadTime);
+        if(onGameRestart != null)
+        {
+            onGameRestart();
+        }
         Gamemanager.instance.LoadLevel(0);
     }
 }
