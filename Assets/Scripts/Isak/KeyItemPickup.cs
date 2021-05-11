1600e5b722238f8bb704008f40d1c5562a43fe37
Viktor Ek <viktor.ek@futuregames.nu>
2021-05-11 12:27:04 +0200

    Pop up for Jetpack, shovel, Pewpew

--- Assets/Scripts/Isak/KeyItemPickup.cs
+++ Assets/Scripts/Isak/KeyItemPickup.cs
@@ -6,10 +6,16 @@ public class KeyItemPickup : PickUp
 {
     public static Events.UnlockKeyItemEvent onKeyItemPickup;
     [SerializeField] KeyItems.Items itemUnlocked;
+    public GameObject itemPopUp;
     public void UnlockItem()
     {
         if(onKeyItemPickup != null)
         {
+            if (GetComponent<Reward>() != null)
+            {
+                UIManager.instance.EnablePopUp(itemPopUp, 1, GetComponent<Reward>().rewardStats.icon);
+            }
+
             onKeyItemPickup(itemUnlocked);
         }
         DestroyObject();
