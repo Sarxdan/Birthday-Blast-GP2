177be548b49f116429dcbdeeac7648bcef49b3a7
Viktor Ek <viktor.ek@futuregames.nu>
2021-05-12 11:03:04 +0200

    Fixed UI bug

--- Assets/Scripts/Isak/Managers/UIManager.cs
+++ Assets/Scripts/Isak/Managers/UIManager.cs
@@ -121,6 +121,8 @@ public class UIManager : Singleton<UIManager>
 
         if (FindObjectOfType<Level>().levelType == LevelType.Island)
         {
+            if (FindObjectOfType<ThirdPersonController>() == false) return;
+            
             if (!toggle && FindObjectOfType<ThirdPersonController>().disableCameraController)
             {
                 ToggleMouse(true);
@@ -152,8 +154,13 @@ public class UIManager : Singleton<UIManager>
         
         ToggleMouse(toggle);
         
+        
         if (FindObjectOfType<Level>().levelType == LevelType.Jetpack) return;
-        FindObjectOfType<ThirdPersonController>().ToggleControls(!toggle);
+
+        if (FindObjectOfType<ThirdPersonController>())
+        {
+            FindObjectOfType<ThirdPersonController>().ToggleControls(!toggle);
+        }
     }
     
     public void OnTogglePause(InputAction.CallbackContext context)
